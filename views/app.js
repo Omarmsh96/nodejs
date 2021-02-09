b283a9ff1cd49c0f0e0912d0581d66d3dd6391db
Omarmsh96 <78327864+Omarmsh96@users.noreply.github.com>
2021-02-09 03:23:38 +0100

    Add files via upload

---
+++ views/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const expsession = require('express-session')
+const mongoose = require('mongoose');
+const jsonweb = require('jsonwebtoken');
+//const MongoClint = require('mongodb').MongoClint
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(expressSession({secret:'omar' ,saveUninitalized: false , resave:false}))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// conncet to database
+mongoose.connect('mongodb://localhost/appex', { useNewUrlParser: true } ,(error) =>{
+ if(error){ console.log('error')}
+ else{
+  console.log('connecting to db')
+  } 
+})
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
